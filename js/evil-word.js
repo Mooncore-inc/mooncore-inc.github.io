e538666fff3df4507828e350cf8e3673af1bc64e
Fazzyt <76790298+Fazzyt@users.noreply.github.com>
2025-04-10 01:06:39 +0300

    1.4
    сделал новый дисигн

---
+++ js/evil-word.js
@@ -0,0 +1,20 @@
+const words = ['inc', 'evil'];
+let currentIndex = 0;
+const wordElement = document.querySelector('.changing-word');
+
+function changeWord() {
+    wordElement.classList.add('fade-out');
+
+    setTimeout(() => {
+        currentIndex = (currentIndex + 1) % words.length;
+        wordElement.textContent = words[currentIndex];
+        wordElement.classList.remove('fade-out');
+        wordElement.classList.add('fade-in');
+    }, 500);
+
+    setTimeout(() => {
+        wordElement.classList.remove('fade-in');
+    }, 1000);
+    }
+
+setInterval(changeWord, 5000);
